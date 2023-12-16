5420018a11a91280a6a4b8b0a0bd3d3889903fab
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2023-12-16 12:14:35 -0600

    first commit

---
+++ src-pos/com/openbravo/pos/scripts/oracle-upgrade-2.00.sql
@@ -0,0 +1,68 @@
+--    Openbravo POS is a point of sales application designed for touch screens.
+--    Copyright (C) 2007-2008 Openbravo, S.L.
+--    http://sourceforge.net/projects/openbravopos
+--
+--    This program is free software; you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation; either version 2 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program; if not, write to the Free Software
+--    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+-- Database upgrade script for ORACLE
+-- v2.00 - v2.10
+
+ALTER TABLE PEOPLE ADD CARD VARCHAR2(255);  
+CREATE INDEX PEOPLE_CARD_INX ON PEOPLE(CARD);
+
+ALTER TABLE CUSTOMERS ADD TAXID VARCHAR2(255);  
+ALTER TABLE CUSTOMERS ADD CARD VARCHAR2(255);  
+ALTER TABLE CUSTOMERS ADD MAXDEBT DOUBLE PRECISION DEFAULT 0 NOT NULL;
+ALTER TABLE CUSTOMERS ADD CURDATE TIMESTAMP;
+ALTER TABLE CUSTOMERS ADD CURDEBT DOUBLE PRECISION;
+CREATE INDEX CUSTOMERS_TAXID_INX ON CUSTOMERS(TAXID);
+CREATE INDEX CUSTOMERS_CARD_INX ON CUSTOMERS(CARD);
+
+ALTER TABLE PRODUCTS ADD ATTRIBUTES BLOB;
+
+ALTER TABLE TICKETS ADD CUSTOMER VARCHAR2(255);
+ALTER TABLE TICKETS ADD CONSTRAINT TICKETS_CUSTOMERS_FK FOREIGN KEY (CUSTOMER) REFERENCES CUSTOMERS(ID);
+CREATE INDEX TICKETS_TICKETID ON TICKETS(TICKETID);
+
+ALTER TABLE TICKETLINES ADD ATTRIBUTES BLOB;
+
+ALTER TABLE RECEIPTS ADD DATENEW TIMESTAMP;
+CREATE INDEX RECEIPTS_INX_1 ON RECEIPTS(DATENEW);
+UPDATE RECEIPTS SET DATENEW = (SELECT DATENEW FROM TICKETS WHERE TICKETS.ID = RECEIPTS.ID);
+ALTER TABLE RECEIPTS MODIFY DATENEW TIMESTAMP NOT NULL;
+
+DROP INDEX TICKETS_INX_1;
+ALTER TABLE TICKETS DROP COLUMN DATENEW;
+
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('14', 'Menu.Root', 0, $FILE{/com/openbravo/pos/templates/Menu.Root.txt});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('15', 'Printer.CustomerPaid', 0, $FILE{/com/openbravo/pos/templates/Printer.CustomerPaid.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('16', 'Printer.CustomerPaid2', 0, $FILE{/com/openbravo/pos/templates/Printer.CustomerPaid2.xml});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('17', 'payment.cash', 0, $FILE{/com/openbravo/pos/templates/payment.cash.txt});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('18', 'banknote.50euro', 1, $FILE{/com/openbravo/pos/templates/banknote.50euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('19', 'banknote.20euro', 1, $FILE{/com/openbravo/pos/templates/banknote.20euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('20', 'banknote.10euro', 1, $FILE{/com/openbravo/pos/templates/banknote.10euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('21', 'banknote.5euro', 1, $FILE{/com/openbravo/pos/templates/banknote.5euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('22', 'coin.2euro', 1, $FILE{/com/openbravo/pos/templates/coin.2euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('23', 'coin.1euro', 1, $FILE{/com/openbravo/pos/templates/coin.1euro.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('24', 'coin.50cent', 1, $FILE{/com/openbravo/pos/templates/coin.50cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('25', 'coin.20cent', 1, $FILE{/com/openbravo/pos/templates/coin.20cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('26', 'coin.10cent', 1, $FILE{/com/openbravo/pos/templates/coin.10cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('27', 'coin.5cent', 1, $FILE{/com/openbravo/pos/templates/coin.5cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('28', 'coin.2cent', 1, $FILE{/com/openbravo/pos/templates/coin.2cent.png});
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('29', 'coin.1cent', 1, $FILE{/com/openbravo/pos/templates/coin.1cent.png});
+
+DELETE FROM SHAREDTICKETS;
+
+UPDATE APPLICATIONS SET NAME = $APP_NAME{}, VERSION = '2.10' WHERE ID = $APP_ID{};
